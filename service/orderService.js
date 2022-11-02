8689f083dc8c717bc2c6601a7f04b1bd6c939bc4
길길성민 <ksm65501@gmail.com>
2022-11-02 13:24:04 +0900

    feat:상세 주문 조회 API 구현
    feat:에러 핸들러 추가

--- service/orderService.js
+++ service/orderService.js
@@ -1,9 +1,18 @@
+const CustomError = require("../middleware/CustomError");
 const { order } = require("../models/");
 
 const selectOrders = async () => {
   return await order.findAll();
 };
 
+const selectOrder = async (orderId) => {
+  const orderInfo = await order.findOne({ where: { order_id: orderId } });
+  if (!orderInfo) throw new CustomError(400);
+
+  return orderInfo;
+};
+
 module.exports = {
   selectOrders,
+  selectOrder,
 };

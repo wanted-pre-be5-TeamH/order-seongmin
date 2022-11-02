572ee644776cb914c49cd90528c8270dbfed2da1
길길성민 <ksm65501@gmail.com>
2022-11-02 19:59:31 +0900

    feat:주문 상태 변경 API 구현

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -4,11 +4,15 @@ const {
   getOrders,
   getOrder,
   getSearch,
+  pathOrderStatus,
 } = require("../controller/orderController");
 const { catchAsync } = require("../middleware/ErrorHandler");
 
 router.route("/").get(getOrders);
 router.route("/search").get(catchAsync(getSearch));
-router.route("/:orderId").get(catchAsync(getOrder));
+router
+  .route("/:orderId")
+  .get(catchAsync(getOrder))
+  .patch(catchAsync(pathOrderStatus));
 
 module.exports = router;
